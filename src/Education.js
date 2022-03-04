3f7380c4bf1ac8bc6ba580aa6371119937ca7e7c
joycepestin <joyceann.pestin.cics@ust.edu.ph>
2022-03-04 13:46:17 +0800

    Education Logo Edited

--- src/Education.js
+++ src/Education.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Education.css'
+import bestLogo from './img/bestlogo.png'
+import cnagLogo from './img/cnag.png'
+import ustLogo from './img/ustlogo.png'
+import rmhsLogo from './img/rmhs.png'
 
 function Education() {
     return (
@@ -12,6 +16,7 @@ function Education() {
             <div className='row'>
                 <div className="column">
                     <div className="card">
+                    <img src={ustLogo} alt="ust" className='org-logo'/>
                         <h2 className='education-heading'>
                             University of Santo Tomas
                         </h2>
@@ -22,6 +27,7 @@ function Education() {
                 </div>
                 <div className="column">
                     <div className="card">
+                    <img src={ustLogo} alt="ust" className='org-logo'/>
                         <h2 className='education-heading'>
                             University of Santo Tomas Senior High School
                         </h2>
@@ -32,6 +38,7 @@ function Education() {
                 </div>
                 <div className="column">
                     <div className="card">
+                    <img src={rmhsLogo} alt="rmhs" className='org-logo'/>
                         <h2 className='education-heading'>
                             Ramon Magsaysay High School Manila
                         </h2>
@@ -40,9 +47,60 @@ function Education() {
                         <p className='education-text'>High School Department</p>
                     </div>
                 </div>
-                
+
             </div>
-        </div>
+
+            <div className='heading'>
+                <div className='education-meta'>
+                    <h1 className='heading-org'>Organization</h1>
+                </div>
+            </div>
+            <div className='row'>
+                <div className="column">
+                    <div className="card-org">
+                        <img src={bestLogo} alt="cnag" className='org-logo'/>
+                        <h2 className='education-heading'>
+                            Becarios de Santo Tomas
+                        </h2>
+                        <h6 className='education-year'>S.Y 2018-2019</h6>
+                        <p className='education-text'>Executive Associate</p>
+                        <p className='education-text'>Sole Thomasian Scholars Association of the University of Santo Tomas</p>
+                    </div>
+                </div>
+                
+                <div className="column">
+                    <div className="card-org">
+                    <img src={cnagLogo} alt="beca" className='org-logo'/>
+                        <h2 className='education-heading'>
+                            Cisco Gateway Academy - UST CICS
+                        </h2>
+                        <h6 className='education-year'>A.Y 2019-2020</h6>
+                        
+                        <p className='education-text'>Vice President for Creatives</p>
+                        <p className='education-text'>College of Information and Computing Sciences</p>
+                    </div>
+                </div>
+
+                <div className="column">
+                    <div className="card-org">
+                    <img src={cnagLogo} alt="beca" className='org-logo'/>
+                        <h2 className='education-heading'>
+                            Cisco Gateway Academy - UST CICS
+                        </h2>
+                        <h6 className='education-year'>A.Y 2020-2021</h6>
+                        
+                        <p className='education-text'>Vice President for Audit and Logistics</p>
+                        <p className='education-text'>College of Information and Computing Sciences</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+            
+
+            
+        </div >
     )
 }
 
